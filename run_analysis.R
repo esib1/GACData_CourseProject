b48c2ddce3b41b556e7c8e017a4798b12535032e
Elliot Butterfield <eb1794@embp.local>
2015-12-26 21:58:41 -0500

    updated run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+
+if (!require("data.table")) {
+  install.packages("data.table")
+}
+
+if (!require("reshape2")) {
+  install.packages("reshape2")
+}
+
+require("data.table")
+require("reshape2")
+
+activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+extract_features <- grepl("mean|std", features)
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+names(X_test) = features
+X_test = X_test[,extract_features]
+y_test[,2] = activityLabels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+names(X_train) = features
+
+X_train = X_train[,extract_features]
+y_train[,2] = activityLabels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+
+data = rbind(test_data, train_data)
+idLabels   = c("subject", "Activity_ID", "Activity_Label")
+dataLabels = setdiff(colnames(data), idLabels)
+meltData      = melt(data, id = idLabels, measure.vars = dataLabels)
+dataTidy   = dcast(meltData, subject + Activity_Label ~ variable, mean)
+
+write.table(dataTidy, row.name = FALSE, file = "./tidy_data_esib1.txt")
