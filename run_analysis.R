129cd9963b5d08660c2e7fb0ea5832fd11529341
Elliot Butterfield <eb1794@embp.local>
2015-12-26 22:05:01 -0500

    updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,6 @@
 
-if (!require("data.table")) {
-  install.packages("data.table")
-}
-
-if (!require("reshape2")) {
-  install.packages("reshape2")
-}
-
+if (!require("data.table")) {install.packages("data.table")}
+if (!require("reshape2")) {install.packages("reshape2")}
 require("data.table")
 require("reshape2")
 
